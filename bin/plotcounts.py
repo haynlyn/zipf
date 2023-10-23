5e5df7703ce44d5942a75d089ca6fdabc0b7545e
Daniel Bellick <dbellick93@gmail.com>
2023-10-23 14:25:28 -0400

    Moved files from exercies into another repo.

--- bin/plotcounts.py
+++ bin/plotcounts.py
@@ -1,4 +1,4 @@
-"""Brief description of what the script does."""
+"""Plot word counts."""
 
 import argparse
 import pandas as pd
@@ -6,7 +6,7 @@ import pandas as pd
 def main(args):
     "Run the program."
     df = pd.read_csv(args.infile, header=None,
-            names=('word', 'word_frequency'))
+                    names=('word', 'word_frequency'))
     df['rank'] = df['word_frequency'].rank(ascending=False,
                                             method='max')
     ax = df.plot.scatter(x='word_frequency', y='rank',
@@ -20,10 +20,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('infile', type=argparse.FileType('r'), nargs='?',
                         default='-', help='Input file name')
+    parser.add_argument('--outfile', default='plotcounts.png', help='Output file name')
     parser.add_argument('--xlim', default=None, type=float,
                         metavar=('XMIN', 'XMAX'), nargs=2,
                         help='X-axis bounds')
-    parser.add_argument('--outfile', default='plotcounts.png', help='Output file name')
     args = parser.parse_args()
     main(args)
 
