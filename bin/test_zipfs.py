bea1eccde5676167e38c1c7a5ddc16dc9b2669ec
Daniel Bellick <dbellick93@gmail.com>
2023-10-29 11:39:28 -0400

    Added tests and continuous integration

--- bin/test_zipfs.py
+++ bin/test_zipfs.py
@@ -6,6 +6,15 @@ import numpy as np
 import plotcounts
 import countwords
 
+def test_regression():
+    """Regression test for Dracula."""
+    with open('data/dracula.txt', 'r') as reader:
+        word_counts_dict = countwords.count_words(reader)
+    counts_array = np.array(list(word_counts_dict.values()))
+    actual_alpha = plotcounts.get_power_law_params(counts_array)
+    expected_alpha = pytest.approx(1.087, abs=0.001)
+    assert actual_alpha == expected_alpha
+
 def test_integration():
     """Test the full word count to alpha parameter workflow."""
     with open('test_data/random_words.txt', 'r') as reader:
