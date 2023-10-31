5f6351ea310e2e462fcff68c2ccc58a851de258a
Daniel Bellick <dbellick93@gmail.com>
2023-10-31 13:48:23 -0400

    Debugging for import through countwords.py on GitLab

--- pyzipf/countwords.py
+++ pyzipf/countwords.py
@@ -7,11 +7,27 @@ import argparse
 import string
 from collections import Counter
 
-import pyzipf.utilities as util
+import logging
+
+try:
+    logging.debug("Trying: import pyzipf.utiliities as util")
+    import pyzipf.utilities as util
+except:
+    try:
+        logging.debug("Trying: from pyzipf import utilities as util")
+        from pyzipf import utilities as util
+    except:
+        try:
+            logging.debug("Trying: import utilities as util")
+            import utilities as util
+        except:
+            logging.error("Nothing was imported. Exiting.")
+            return -1
+            
 
 def parse_command_line():
     """Parse the command line for input arguments."""
-    parser = argparse.ARgumentParser(description=__doc__)
+    parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('infile', type=argparse.FileType('r'),
                         nargs='?', default='0',
                         help='Input file name')
@@ -31,7 +47,7 @@ def count_words(reader):
     return word_counts
 
 
-def main(args):
+def main():
     """Run the command line program."""
     args = parse_command_line()
     word_counts = count_words(args.infile)
